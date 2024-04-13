70ee8edb4fc0a5d6fae1d64358beda3428e1f903
WickerPrison <cobylax17@gmail.com>
2024-04-13 17:30:36 -0600

    figured out how to read in perlin noise

--- shader.js
+++ shader.js
@@ -22,11 +22,11 @@ window.addEventListener("resize", () =>{
 let vShader = vertShader.innerHTML;
 let fShader = fragShader.innerHTML;
 let uniforms = {
-    delta: 0
+    delta: 0,
+    perlin: PIXI.Texture.from("https://imgs.search.brave.com/sksLJoDCQW0RHsKZKmH6UW5dUbikM3TcnX8zPfiID0M/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pLnN0/YWNrLmltZ3VyLmNv/bS9pMDB5eC5wbmc")
 };
 
 const myFilter = new PIXI.Filter(vShader, fShader, uniforms);
-
 
 const texture = PIXI.Texture.from("https://imgs.search.brave.com/QCfVHGlr0akNgT0CWD4NRn2b09CvBRhyAiomYnKgYEM/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMud2lraWEubm9j/b29raWUubmV0L2Zp/Y3Rpb25hbGNyb3Nz/b3Zlci9pbWFnZXMv/MC8wMC9JbnZpbmNp/YmxlX0ZvcnRuaXRl/LndlYnAvcmV2aXNp/b24vbGF0ZXN0L3Nj/YWxlLXRvLXdpZHRo/LWRvd24vMjAwP2Ni/PTIwMjMxMTIyMjE1/NzQx");
 const img = new PIXI.Sprite(texture);
@@ -37,13 +37,12 @@ img.filters = [myFilter]
 
 app.stage.addChild(img);
 
-
 app.ticker.add(animate);
 
 let delta = 0;
 function animate(){
     img.x = app.renderer.screen.width / 2;
     img.y = app.renderer.screen.height / 2;
-    delta += 0.1;
-    uniforms.delta = 0.5 + Math.sin(delta) * 0.5;
+    delta += 0.01;
+    uniforms.delta = delta;
 }
